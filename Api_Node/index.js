568f193ae06b11ec469661a85ab6c3ab10936260
danielhennig <danielhennig10@gmail.com>
2025-03-26 13:29:31 -0300

    adiciona dependência do Express e corrige resposta do endpoint /curso

--- Api_Node/index.js
+++ Api_Node/index.js
@@ -4,9 +4,8 @@ const server = express();
 
 // localhost:3000/curso
 server.get('/curso', (req, res) => {
-  
-  return res.json({ curso: 'Node JS'});
 
-})
+  return res.send({ curso: 'Node.js' });
 
+});
 server.listen(3000);
