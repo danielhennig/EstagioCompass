a112799f1aff1d475b2f031181e10ff4656a1865
danielhennig <danielhennig10@gmail.com>
2025-03-31 09:05:46 -0300

    adiciona conexão com o MongoDB usando Mongoose e atualiza as dependências no package.json

--- DevHouse/src/app.js
+++ DevHouse/src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import routes from './routes.js';
+import mongoose from 'mongoose';
 
 class App {
 
     constructor() {
         this.server = express();
+
+        mongoose.connect('mongodb+srv://danielhennig:devhouse@devhouse.13ru6ue.mongodb.net/?retryWrites=true&w=majority&appName=devHouse',
+            { useNewUrlParser: true, useUnifiedTopology: true });
 
         this.middlewares();
         this.routes();
